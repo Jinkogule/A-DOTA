8aec43cc94307f8fbb0784253656ea7574ac1b7d
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-04-25 14:56:57 -0300

    Tratando dados inválidos, etc
    Tratando dados inválidos, padronizando os dados antes de serem enviados ao banco de dados e inserindo os novos atributos adicionados.

--- src/dao/AnimaisDao.java
+++ src/dao/AnimaisDao.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
-
 
 import interfaces.InterfaceDao;
 import java.sql.PreparedStatement;
@@ -12,10 +6,8 @@ import java.sql.SQLException;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import modelo.AnimaisModelo;
-/**
- *
- * @author Lucas Couto
- */
+import static views.FormPadrao.padronizaString;
+
 public class AnimaisDao implements InterfaceDao{
     
     String sql;
@@ -24,23 +16,51 @@ public class AnimaisDao implements InterfaceDao{
     @Override
     public void salvarDao(Object... valor) {
         AnimaisModelo am = (AnimaisModelo) valor[0];
+        
+        //padroniza (na medida do possível) os dados que serão enviados para o BD
+        String Nome_Animal = padronizaString(am.getNome_Animal());
+        String Raca = padronizaString(am.getRaca());
+        String Cor = padronizaString(am.getCor());
         //inclui novo se for inclusão ou altera se for alteração
         if (am.getId() == 0){
-            sql = "INSERT INTO animais (Nome_Animal, Raca, Idade, Porte) VALUES (?, ?, ?, ?)";
+            sql = "INSERT INTO animais (Nome_Animal, Tipo, Raca, Idade, Porte, Cor) VALUES (?, ?, ?, ?, ?, ?)";
         }
         else{
-            sql = "UPDATE animais SET nome=Nome_Animal WHERE Id=?";
+            sql = "UPDATE animais SET nome=Nome_Abrigo WHERE Id=?";
         }
-        
+
         try{
             stm = ConnectDB.abreConexao().prepareStatement(sql);
-            stm.setString(1, am.getNome_Animal());
-            stm.setString(2, am.getRaca());
-            stm.setInt(3, am.getIdade());
-            stm.setString(4, am.getPorte());
+            stm.setString(1, Nome_Animal);
+            stm.setString(2, am.getTipo());
+            stm.setString(3, Raca);
+            stm.setInt(4, am.getIdade());
+            stm.setString(5, am.getPorte());
+            stm.setString(6, Cor);
             if (am.getId()>0){
                 stm.setInt(2, am.getId());
             }
+            
+            //Tratando ocorrência de dados inválidos
+            if (!(am.getNome_Animal().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - çü - ü]+")) || am.getNome_Animal().isBlank()){
+                throw new IllegalArgumentException("Nome inválido.");
+            }
+            if (am.getTipo() == ""){
+                throw new IllegalArgumentException("Insira o valor de TIPO.");
+            }
+            if (!(am.getRaca().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Animal().isBlank()){
+                throw new IllegalArgumentException("Raça inválida.");
+            }
+            if (am.getIdade() < 0){
+                throw new IllegalArgumentException("Idade inválida.");
+            }
+            if (am.getPorte() == ""){
+                throw new IllegalArgumentException("Insira o valor de PORTE.");
+            }
+            if (!(am.getCor().matches("[A-Za-z - ]+")) || am.getNome_Animal().isBlank()){
+                throw new IllegalArgumentException("Cor inválida.");
+            }
+            
             stm.execute();
             stm.close();
             JOptionPane.showMessageDialog(null, "Cadastro Registrado.");
@@ -48,15 +68,9 @@ public class AnimaisDao implements InterfaceDao{
         catch (Exception e){
             JOptionPane.showMessageDialog(null, "Erro: "+e);
         }
-        am.getNome_Animal();
-        am.getRaca();
-        am.getIdade();
-        am.getPorte();
-        
-    
     }
-    
-        @Override
+
+    @Override
     public void excluirDao(int id) {
         
     }
@@ -69,6 +83,5 @@ public class AnimaisDao implements InterfaceDao{
     @Override
     public void carregarComboBoxDao(JComboBox itens) throws SQLException {
         
-    }
- 
+    } 
 }
