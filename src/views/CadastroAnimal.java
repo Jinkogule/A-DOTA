53ba3de0681763305a9d032dd122f784d55beb71
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-04-25 14:41:47 -0300

    Organizando o Form, etc
    Organizando o formulário, incluindo novos campos no "salvar" e preenchendo os JComboBox,

--- src/views/CadastroAnimal.java
+++ src/views/CadastroAnimal.java
@@ -4,8 +4,11 @@ import controle.AnimaisControle;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-        
+
 public class CadastroAnimal extends FormPadrao{
+    
+    JLabel jlTipo;
+    JComboBox jcbTipo;
     
     JLabel jlRaca;
     JTextField jtfRaca;
@@ -16,38 +19,66 @@ public class CadastroAnimal extends FormPadrao{
     JLabel jlPorte;
     JComboBox jcbPorte;
     
+    JLabel jlCor;
+    JTextField jtfCor;
+    
     public CadastroAnimal(){
         setTitle("Cadastro de Animal");
     }
-    
+
     @Override
     public void iniciarComponentes() {
-        //Componentes de "Raça do Animal"
-        jlRaca = new JLabel("Raça do Animal");
-        jlRaca.setBounds(10, 80, 200, 25);
+        //Componentes de "Tipo do animal"
+        jlTipo = new JLabel("Tipo");
+        jlTipo.setBounds(300, 140, 75, 25);
+        jPanel2.add(jlTipo);
+        
+        jcbTipo = new JComboBox();
+        jcbTipo.setBounds(300, 160, 150, 25);
+        jPanel2.add(jcbTipo);
+        jcbTipo.addItem("");
+        jcbTipo.addItem("Gato");
+        jcbTipo.addItem("Cachorro");
+        
+        //Componentes de "Raça do animal"
+        jlRaca = new JLabel("Raça");
+        jlRaca.setBounds(10, 80, 260, 25);
         jPanel2.add(jlRaca);
         
         jtfRaca = new JTextField();
-        jtfRaca.setBounds(10, 100, 200, 25);
+        jtfRaca.setBounds(10, 100, 260, 25);
         jPanel2.add(jtfRaca);
         
-        //Componentes de "Idade do Animal"
-        jlIdade = new JLabel("Idade do Animal");
-        jlIdade.setBounds(300, 80, 50, 25);
+        //Componentes de "Idade do animal"
+        jlIdade = new JLabel("Idade");
+        jlIdade.setBounds(220, 140, 50, 25);
         jPanel2.add(jlIdade);
         
         jtfIdade = new JTextField();
-        jtfIdade.setBounds(300, 100, 100, 25);
+        jtfIdade.setBounds(220, 160, 50, 25);
         jPanel2.add(jtfIdade);
         
         //Componentes de "Porte do Animal"
-        jlPorte = new JLabel("Porte do Animal");
-        jlPorte.setBounds(530, 80, 75, 25);
+        jlPorte = new JLabel("Porte");
+        jlPorte.setBounds(300, 80, 75, 25);
         jPanel2.add(jlPorte);
         
         jcbPorte = new JComboBox();
-        jcbPorte.setBounds(530, 100, 75, 25);
+        jcbPorte.setBounds(300, 100, 150, 25);
         jPanel2.add(jcbPorte);
+        jcbPorte.addItem("");
+        jcbPorte.addItem("Pequeno");
+        jcbPorte.addItem("Médio");
+        jcbPorte.addItem("Grande");
+        
+        //Componentes de "Cor do animal"
+        jlCor = new JLabel("Cor");
+        jlCor.setBounds(10, 140, 200, 25);
+        jPanel2.add(jlCor);
+        
+        jtfCor = new JTextField();
+        jtfCor.setBounds(10, 160, 200, 25);
+        jPanel2.add(jtfCor);
         
     }
     
@@ -55,21 +86,26 @@ public class CadastroAnimal extends FormPadrao{
 
     @Override
     public void salvar() {
-        controle.salvarControle(jtfId.getText(), jtfNome.getText(), jtfRaca.getText(), jtfIdade.getText());
+        controle.salvarControle(jtfId.getText(), jtfNome.getText(), jcbTipo.getSelectedItem(), jtfRaca.getText(), jtfIdade.getText(), jcbPorte.getSelectedItem(), jtfCor.getText());
     }
 
     @Override
     public void habilitaCampos(boolean estado) {
         jtfNome.setEnabled(estado);
+        jcbTipo.setEnabled(estado);
         jtfRaca.setEnabled(estado);
-        jcbPorte.setEnabled(estado);
         jtfIdade.setEnabled(estado);
+        jcbPorte.setEnabled(estado);
+        jtfCor.setEnabled(estado);
     }
 
     @Override
     public void limpaCampo() {
         jtfNome.setText("");
+        jcbTipo.setSelectedIndex(0);
         jtfRaca.setText("");
         jtfIdade.setText("");
+        jcbPorte.setSelectedIndex(0);
+        jtfCor.setText("");
     }
 }
