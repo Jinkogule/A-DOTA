f4059f88af382b5cbd0c0347943ed472fcafb1c4
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-04-25 14:55:31 -0300

    Tratando dados inválidos, etc
    Tratando dados inválidos, padronizando os dados antes de serem enviados ao banco de dados e inserindo os novos atributos adicionados.

--- src/dao/AbrigoDao.java
+++ src/dao/AbrigoDao.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import modelo.AbrigoModelo;
+import static views.FormPadrao.padronizaString;
 
 public class AbrigoDao implements InterfaceDao{
     
@@ -15,9 +16,15 @@ public class AbrigoDao implements InterfaceDao{
     @Override
     public void salvarDao(Object... valor) {
         AbrigoModelo am = (AbrigoModelo) valor[0];
+        
+        //padroniza (na medida do possível) os dados que serão enviados para o BD
+        String Nome_Abrigo = padronizaString(am.getNome_Abrigo());
+        String Nome_Dono = padronizaString(am.getNome_Dono());
+        String Endereco = padronizaString(am.getEndereco());
+        String CEP = padronizaString(am.getCEP());
         //inclui novo se for inclusão ou altera se for alteração
         if (am.getId() == 0){
-            sql = "INSERT INTO abrigo (Nome_Abrigo, Nome_Dono, Endereco, CEP) VALUES (?, ?, ?, ?)";
+            sql = "INSERT INTO abrigo (Nome_Abrigo, Nome_Dono, Estado, Cidade, Endereco, CEP) VALUES (?, ?, ?, ?, ?, ?)";
         }
         else{
             sql = "UPDATE abrigo SET nome=Nome_Abrigo WHERE Id=?";
@@ -25,13 +32,33 @@ public class AbrigoDao implements InterfaceDao{
         
         try{
             stm = ConnectDB.abreConexao().prepareStatement(sql);
-            stm.setString(1, am.getNome_Abrigo());
-            stm.setString(2, am.getNome_Dono());
-            stm.setString(3, am.getEndereco());
-            stm.setString(4, am.getCEP());
+            stm.setString(1, Nome_Abrigo);
+            stm.setString(2, Nome_Dono);
+            stm.setString(3, am.getEstado());
+            stm.setString(4, am.getCidade());
+            stm.setString(5, Endereco);
+            stm.setString(6, CEP);
             if (am.getId()>0){
                 stm.setInt(2, am.getId());
             }
+            
+            //tratando ocorrência de dados inválidos
+            if (!(am.getNome_Abrigo().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Abrigo().isBlank()){
+                throw new IllegalArgumentException("Nome do abrigo inválido.");
+            }
+            if (!(am.getNome_Dono().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Dono().isBlank()){
+                throw new IllegalArgumentException("Nome do dono inválido.");
+            }
+            if (am.getEstado() == ""){
+                throw new IllegalArgumentException("Insira o valor de ESTADO.");
+            }
+            if (am.getCidade() == ""){
+                throw new IllegalArgumentException("Insira o valor de CIDADE.");
+            }
+            if (!(am.getEndereco().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç, - ,]+")) || am.getEndereco().isBlank()){
+                throw new IllegalArgumentException("Endereço inválido.");
+            }
+            
             stm.execute();
             stm.close();
             JOptionPane.showMessageDialog(null, "Cadastro Registrado.");
@@ -39,13 +66,6 @@ public class AbrigoDao implements InterfaceDao{
         catch (Exception e){
             JOptionPane.showMessageDialog(null, "Erro: "+e);
         }
-        am.getNome_Abrigo();
-        am.getNome_Dono();
-        am.getEndereco();
-        am.getCEP();
-        
-   
-        
     }
 
     @Override
@@ -61,6 +81,5 @@ public class AbrigoDao implements InterfaceDao{
     @Override
     public void carregarComboBoxDao(JComboBox itens) throws SQLException {
         
-    }
-    
+    } 
 }
