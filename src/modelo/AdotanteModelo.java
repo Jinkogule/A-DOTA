117fab8c9cb8620bacaa515631d1faba036e57c4
Gabriel Barbedo Fernandes <gbarbedo@id.uff.br>
2021-04-25 04:06:40 -0300

    AdotanteDao

--- src/modelo/AdotanteModelo.java
+++ src/modelo/AdotanteModelo.java
@@ -5,7 +5,6 @@ public class AdotanteModelo {
     private int Id;
     private String NomeAdotante;
     private String IdadeAdotante;
-    private String SexoAdotante;
     private String EnderecoAdotante;
     private String TelefoneAdotante;
     private String CEP;
@@ -14,17 +13,13 @@ public class AdotanteModelo {
     public int getId() {
         return Id;
     }
-
     public void setId(int Id) {
         this.Id = Id;
     }
-
-
     //Nome Adotante
     public String getNomeAdotante() {
         return NomeAdotante;
     }
-
     public void setNomeAdotante(String NomeAdotante) {
         this.NomeAdotante = NomeAdotante;
     }
@@ -35,48 +30,25 @@ public class AdotanteModelo {
         public void setIdadeAdotante(String IdadeAdotante) {
         this.IdadeAdotante = IdadeAdotante;
     }
-    //Sexo Adotante
-    public String getSexoAdotante() {
-        return SexoAdotante;
-    }
-
-    public void setSexoAdotante(String SexoAdotante) {
-        this.SexoAdotante = SexoAdotante;
-    }
-
     //Endereço Adotante
     public String getEnderecoAdotante() {
         return EnderecoAdotante;
     }
-
     public void setEnderecoAdotante(String EnderecoAdotante) {
         this.EnderecoAdotante = EnderecoAdotante;
     }
-
-
-
     //CEP Adotante
     public String getCEP() {
         return CEP;
     }
-
     public void setCEP(String CEP) {
         this.CEP = CEP;
     }
-
     //Telefone Adotante
     public String getTelefoneAdotante() {
         return TelefoneAdotante;
     }
-
     public void setTelefoneAdotante(String TelefoneAdotante) {
         this.TelefoneAdotante = TelefoneAdotante;
     }
-
-
-
-
-
-
 }
-
