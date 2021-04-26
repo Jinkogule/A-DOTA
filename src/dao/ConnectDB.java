f0674edc35e23d81aaf33105d8c05eb5aabfcc14
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-04-26 11:32:26 -0300

    Reformulação da estrutura da classe
    tornando o método "abreconexão" não estático

--- src/dao/ConnectDB.java
+++ src/dao/ConnectDB.java
@@ -1,27 +1,44 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 public class ConnectDB {
+    public Statement stm;
+    public ResultSet rs;
+    public Connection con;
     private static final String driverClass = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/a_dota?useTimezone=true&serverTimezone=UTC";
     private static final String usuario = "root";
     private static final String senha = "";
     
-    public static Connection abreConexao(){
-        Connection conexao = null;
-        
+    public void abreConexao(){
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conexao = DriverManager.getConnection(URL, usuario, senha);
+            Class.forName(driverClass);
+            con = DriverManager.getConnection(URL, usuario, senha);
         }
         catch (Exception e){
             JOptionPane.showMessageDialog(null, "Falha na conexão com o banco de dados: "+e);
-        }
-        
-        return conexao;
+        }    
     }
     
+    public void executaSQL(String sql){
+
+        try{
+            stm = con.createStatement(rs.TYPE_SCROLL_INSENSITIVE,rs.CONCUR_READ_ONLY);
+            rs = stm.executeQuery(sql);
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao executar a query: "+e);
+        }
+    }
+    
+    public void desconecta(){
+        try {
+            con.close();
+            JOptionPane.showMessageDialog(null, "BD desconectado com sucesso.");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao encerrar conexão com o banco de dados: "+e);
+        }       
+    }    
 }
