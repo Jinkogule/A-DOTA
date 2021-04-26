2aa21202643c04d61494e005f164183a7415dfa7
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-04-26 11:44:56 -0300

    Adaptando à nova estrutura da conexão
    além de outras mudanças secundarias

--- src/dao/AbrigoDao.java
+++ src/dao/AbrigoDao.java
@@ -12,6 +12,7 @@ public class AbrigoDao implements InterfaceDao{
     
     String sql;
     PreparedStatement stm;
+    ConnectDB conexao = new ConnectDB();
 
     @Override
     public void salvarDao(Object... valor) {
@@ -21,7 +22,6 @@ public class AbrigoDao implements InterfaceDao{
         String Nome_Abrigo = padronizaString(am.getNome_Abrigo());
         String Nome_Dono = padronizaString(am.getNome_Dono());
         String Endereco = padronizaString(am.getEndereco());
-        String CEP = padronizaString(am.getCEP());
         //inclui novo se for inclusão ou altera se for alteração
         if (am.getId() == 0){
             sql = "INSERT INTO abrigo (Nome_Abrigo, Nome_Dono, Estado, Cidade, Endereco, CEP) VALUES (?, ?, ?, ?, ?, ?)";
@@ -31,34 +31,37 @@ public class AbrigoDao implements InterfaceDao{
         }
         
         try{
-            stm = ConnectDB.abreConexao().prepareStatement(sql);
+            conexao.abreConexao();
+            stm = conexao.con.prepareStatement(sql);
             stm.setString(1, Nome_Abrigo);
             stm.setString(2, Nome_Dono);
             stm.setString(3, am.getEstado());
             stm.setString(4, am.getCidade());
             stm.setString(5, Endereco);
-            stm.setString(6, CEP);
+            stm.setString(6, am.getCEP());
             if (am.getId()>0){
                 stm.setInt(2, am.getId());
             }
             
             //tratando ocorrência de dados inválidos
-            if (!(am.getNome_Abrigo().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Abrigo().isBlank()){
+            if (!(am.getNome_Abrigo().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Abrigo().isBlank() || am.getNome_Abrigo().isEmpty()){
                 throw new IllegalArgumentException("Nome do abrigo inválido.");
             }
             if (!(am.getNome_Dono().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç]+")) || am.getNome_Dono().isBlank()){
                 throw new IllegalArgumentException("Nome do dono inválido.");
             }
-            if (am.getEstado() == ""){
+            if ("".equals(am.getEstado())){
                 throw new IllegalArgumentException("Insira o valor de ESTADO.");
             }
-            if (am.getCidade() == ""){
+            if ("".equals(am.getCidade())){
                 throw new IllegalArgumentException("Insira o valor de CIDADE.");
             }
             if (!(am.getEndereco().matches("[A-Za-z - ã - ãá - áâ - âó - óô - ôó - óí - íú - úç - ç, - ,]+")) || am.getEndereco().isBlank()){
                 throw new IllegalArgumentException("Endereço inválido.");
             }
-            
+            if (!(am.getCEP().matches("[0-9---]+")) || am.getCEP().isBlank()){
+                throw new IllegalArgumentException("CEP inválido.");
+            }            
             stm.execute();
             stm.close();
             JOptionPane.showMessageDialog(null, "Cadastro Registrado.");
